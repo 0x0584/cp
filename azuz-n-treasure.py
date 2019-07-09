67c88d63cfd3b2f7d3066ca64ff1977edd320c49
Anas Rchid <rchid.anas@gmail.com>
2019-07-09 04:56:53 +0100

    finish azuz and the fucking corridors...

--- azuz-n-treasure.py
+++ azuz-n-treasure.py
@@ -1,10 +1,13 @@
 #              File:    azuz-n-treasure.py
 #            Author:    Anas
-#       Description:    finding the path through the treasure, which is
-#                       the deepest room
+#       Description:    the idea is that we have to collect a list of all
+#                       possible paths, keeping tack of only new corridros.
+#                       and keep going deeper using recursion until we
+#                       reach a corridor with no next, we return the paths.
+#                       finally, we'll have to check for traps.
 #
 #         Created: <2019-07-08 Mon 23:36:21>
-#         Updated: <2019-07-09 Tue 02:29:25>
+#         Updated: <2019-07-09 Tue 04:51:28>
 #
 # B. #247588
 
@@ -15,38 +18,51 @@ def find_next(corris, corr):
             lst.append(e)
     return lst
 
-final_paths = []
 def solve(corris, paths = [[[0, 1]]]):
     new_paths = []
     has_new_next = False
 
-    for p in paths:
-        for e in find_next(corris, p[-1]):
-            if e not in p:
+    l = list(paths)
+    for path in paths:
+        for e in find_next(corris, path[-1]):
+            if e not in path:
                 has_new_next = True
-                p.append(e)
-                print " p >> ", p
-                new_paths.append(p)
-                print "new paths: ", new_paths
-
+                new_paths.append(path + [e])
     if not has_new_next:
-        final_paths.append(new_paths)
+        return list(paths)
+    return solve(corris, new_paths)
+
+def treasure(solu):
+    last_room = solu[0][-1][1]
+    is_trap = False
+    i = 0
+
+    while i < len(solu):
+        if solu[i][-1][1] != last_room:
+            is_trap = True
+            break
+        i += 1
+
+    if not is_trap:
+        print last_room
     else:
-        solve(corris, new_paths)
+        print "it was a trap!"
 
-corris = [
-    [1, 2],
-    [1, 3],
-    [2, 3],
-    [3, 4],
-    [1, 5]
-]
+def run_tests(fname):
+    index = 0
+    with open(fname) as f:
+        content = f.readlines()
+    content = [x.strip() for x in content]
 
-print corris
-solve(corris)
-print "final", final_paths
+    for i in range(int(content[0])):
+        corris = []
+        index += 1
+        n_corris = int(content[index]) - 1
+        index += 1
+        for j in range(n_corris):
+            corris.append(map(int, content[j + index].split()))
+        index += j
+        treasure(solve(corris))
 
-# start at index 1
-# find next rooms
-# got to each next room
-#
+
+run_tests("azuz.in")
