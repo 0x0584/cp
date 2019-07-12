a294487571273c43fe26d3082da8643f5ce34c54
Anas Rchid <archid-@student.1337.ma>
2019-07-12 09:08:13 +0100

    solve problem H. but the solution is too slow!!

---
+++ omar-n-puzzle.py
@@ -0,0 +1,56 @@
+#              File:    omar-n-puzzle.py
+#            Author:    Anas
+#
+#    Created: <2019-07-11 Wed 20:08:07>
+#    Updated: <2019-07-12 Fri 09:07:46>
+#
+# Thoughts: loop over the points, each time, push a point into a range r
+# if the range still has space, and the diff is true. mark the point as
+# picked. loop over again.
+#
+# H. #247588
+
+def diff(lst, k):
+    if not lst or len(lst) == 0:
+        return False
+    elif len(lst) == 1:
+        return True
+    for i in range(len(lst) - 1):
+        if lst[i + 1] - lst[i] > k:
+            return False
+        i += 1
+    return True
+
+def range_of_points(points, c, k):
+    picked = [False for e in points]
+    ranges = [[]]
+    flag = True
+
+    ranges[0].append(points[0])
+    points.remove(points[0])
+    while flag:
+        flag = False
+        for i in range(len(points)):
+            if not picked[i]:
+                flag = True
+                for r in ranges:
+                    if len(r) < c and diff(r + [points[i]], k):
+                        picked[i] = True
+                        r.append(points[i])
+                        break
+                if not picked[i]:
+                    ranges.append([])
+                    ranges[-1].append(points[i])
+                    picked[i] = True
+    return ranges
+
+def run_tests():
+    n_tests = int(input())
+    tests = []
+    for t in range(n_tests):
+        tests.append([map(int, raw_input().split()), # reading N, C and K
+                      map(int, raw_input().split())])
+    for t in tests:
+        print len(range_of_points(t[1], t[0][1], t[0][2]))
+
+run_tests()
